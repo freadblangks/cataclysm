fa4006b6ce14a83dd5de2d420181430cbe4c0b61
Pagani Walter <paganiwalter@gmail.com>
2022-07-23 08:01:08 -0300

    Update action Ubuntu 22.10

--- apps/ci/ci-compile.sh
+++ apps/ci/ci-compile.sh
@@ -10,7 +10,7 @@ time test -d _install || mkdir _install
 time cd _build
 
 # Run CMake Configurations
-time cmake .. -DCMAKE_INSTALL_PREFIX=../_install -DBUILD_TOOLS:BOOL=1 -DBUILD_SERVER:BOOL=1 -DBUILD_AUTH:BOOL=1 -DSOAP:BOOL=1 -DSCRIPT_LIB_ELUNA:BOOL=1 -DSCRIPT_LIB_SD3:BOOL=1 -DPLAYERBOTS:BOOL=1 -DUSE_STORMLIB:BOOL=1
+time cmake ../ -DCMAKE_INSTALL_PREFIX=../_install -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DWITH_WARNINGS=1 -DTOOLS=0 -DSCRIPTS=static -DMODULES=static
 
 # Compile the Project
 time make -j 6
