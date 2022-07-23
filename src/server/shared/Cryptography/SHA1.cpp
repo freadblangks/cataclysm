f918a0450edeaee61016e124fd861847c9a99f99
Pagani Walter <paganiwalter@gmail.com>
2022-07-23 05:55:09 -0300

    Core/Crypto: Transitional Cryptography update for OpenSSL 1.1
    Support for both OpenSSL 1.0 LTS and OpenSSL 1.1 versions.
    
    Many Linux distributions are still on 1.0 and will stay on LTS for quite
    some time.
    
    Port of CMaNGOS commit: cmangos/mangos-wotlk@e1b0048

--- src/server/shared/Cryptography/SHA1.cpp
+++ src/server/shared/Cryptography/SHA1.cpp
@@ -23,6 +23,7 @@
 SHA1Hash::SHA1Hash()
 {
     SHA1_Init(&mC);
+    memset(mDigest, 0, SHA_DIGEST_LENGTH * sizeof(uint8));
 }
 
 SHA1Hash::~SHA1Hash()
@@ -30,12 +31,12 @@ SHA1Hash::~SHA1Hash()
     SHA1_Init(&mC);
 }
 
-void SHA1Hash::UpdateData(const uint8 *dta, int len)
+void SHA1Hash::UpdateData(const uint8* dta, int len)
 {
     SHA1_Update(&mC, dta, len);
 }
 
-void SHA1Hash::UpdateData(const std::string &str)
+void SHA1Hash::UpdateData(const std::string& str)
 {
     UpdateData((uint8 const*)str.c_str(), str.length());
 }
@@ -49,7 +50,7 @@ void SHA1Hash::UpdateBigNumbers(BigNumber* bn0, ...)
     bn = bn0;
     while (bn)
     {
-        UpdateData(bn->AsByteArray(), bn->GetNumBytes());
+        UpdateData(bn->AsByteArray().get(), bn->GetNumBytes());
         bn = va_arg(v, BigNumber*);
     }
     va_end(v);
@@ -64,4 +65,3 @@ void SHA1Hash::Finalize(void)
 {
     SHA1_Final(mDigest, &mC);
 }
-
